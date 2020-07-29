b23d0ae96998a370dae04609f72a1d3c486f04ef
yattishr <yattish@gmail.com>
2020-07-29 12:35:20 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+// index.js
+
+/**
+ * Required External Modules
+ */
+const express = require("express");
+const path = require("path");
+
+
+/**
+ * App Variables
+ */
+const app = express();
+const port = process.env.PORT || "8000";
+
+
+/**
+ *  App Configuration
+ */
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
+
+
+/**
+ * Routes Definitions
+ */
+// app.get("/", (req, res) => {
+//     res.status(200).send("WHATABYTE: Food For Devs");
+//   });
+app.get("/", (req, res) => {
+    res.render("index", { title: "Home" });
+});
+
+
+app.get("/user", (req, res) => {
+    res.render("user", { title: "Profile", userProfile: { nickname: "Auth0" } });
+});  
+
+app.get("/success_msg", (req, res) => {
+    res.render("success_msg", { title: "Home" });
+});
+  
+
+/**
+ * Server Activation
+ */
+app.listen(port, () => {
+    console.log(`Yattish, listening to requests on http://localhost:${port}`);
+  });
